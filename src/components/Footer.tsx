be2872c6d319c41c4388b4857f7ec5d36d34a354
MrCaro <36524539+MrCaro@users.noreply.github.com>
2026-01-02 13:33:25 -0500

    fix: footer copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,8 +33,7 @@ export function Footer() {
                 {/* <NavLink href="/uses">Uses</NavLink> */}
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Charlie Caro. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Charlie Caro.
               </p>
             </div>
           </ContainerInner>
